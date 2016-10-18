9381445a0c182051154814ef6b7ae052bb69095d
Slava Tarbinskyi <slava0tarbinskiy@outlook.com>
2016-10-18 17:24:47 +0300

    log4net

--- wsdp/BAL/Manager/BaseManager.cs
+++ wsdp/BAL/Manager/BaseManager.cs
@@ -1,16 +1,20 @@
 ﻿using DAL.Interface;
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BAL.Manager {
-	public class BaseManager {
-		protected IUnitOfWork uOW;
-
-		public BaseManager(IUnitOfWork uOW) {
-			this.uOW = uOW;
-		}
-	}
+namespace BAL.Manager
+{
+    public class BaseManager
+    {
+        protected IUnitOfWork uOW;
+        static readonly ILog logger = LogManager.GetLogger("RollingLogFileAppender");
+        public BaseManager(IUnitOfWork uOW)
+        {
+            this.uOW = uOW;
+        }
+    }
 }
