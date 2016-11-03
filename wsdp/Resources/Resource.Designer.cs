f9a0a0881a09eedc2e962ba1a9995b98631651d7
YuraPostevka <yurapostevka@gmail.com>
2016-11-03 16:56:11 +0200

    add translation for downloadpage

--- wsdp/Resources/Resource.Designer.cs
+++ wsdp/Resources/Resource.Designer.cs
@@ -178,6 +178,15 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Download.
+        /// </summary>
+        public static string Download {
+            get {
+                return ResourceManager.GetString("Download", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Edit.
         /// </summary>
         public static string Edit {
