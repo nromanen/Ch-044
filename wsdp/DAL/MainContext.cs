754141046ccbd176f82d03a2e1ea2fc43c02ac82
Nataliia Romanenko <nromanen@softserveinc.com>
2016-11-04 13:40:46 +0200

    push last version

--- wsdp/DAL/MainContext.cs
+++ wsdp/DAL/MainContext.cs
@@ -11,7 +11,7 @@ namespace DAL
     public class MainContext : DbContext
     {
         public MainContext()
-            : base("YurasShop")
+            : base("MyShopNew")
         {
             this.Configuration.LazyLoadingEnabled = true;
 
