cb62ca7335dc74b28faeed107ff2a05ef9780c3a
Slava Tarbinskyi <slava0tarbinskiy@outlook.com>
2016-11-06 11:43:24 +0200

    Updated AddProperty and UpdateProperty Views.

--- wsdp/DAL/MainContext.cs
+++ wsdp/DAL/MainContext.cs
@@ -11,7 +11,7 @@ namespace DAL
     public class MainContext : DbContext
     {
         public MainContext()
-            : base("MyShopNew")
+            : base("SlavaCon")
         {
             this.Configuration.LazyLoadingEnabled = true;
 
