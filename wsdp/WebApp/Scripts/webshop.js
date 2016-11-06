d2cafde66cf61c6a378a3d342e9ecbb0672faee7
alisaanosova <alisaanosova@mail.ru>
2016-11-06 04:56:01 +0200

    adding webshop js file

---
+++ wsdp/WebApp/Scripts/webshop.js
@@ -0,0 +1,11 @@
+﻿//To create popup
+$(function () {
+    $(".webShopItem").click(function (e) {
+
+        e.preventDefault();
+        $.get(this.href, function (data) {
+            $('#dialogContent').html(data);
+            $('#modDialog').modal('show');
+        });
+    });
+});
