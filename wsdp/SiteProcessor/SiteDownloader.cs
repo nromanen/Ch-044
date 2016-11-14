619421943f244fc4526fa4d4ded8c6c91e72bbfe
StasYakhnenko <ghosteagle07@gmail.com>
2016-11-14 21:54:03 +0200

    working with task #32

--- wsdp/SiteProcessor/SiteDownloader.cs
+++ wsdp/SiteProcessor/SiteDownloader.cs
@@ -15,49 +15,56 @@ namespace SiteProcessor
     {
         private const int CONTROL_PORT = 9051;
         private const string PATH = @"Tor\Tor\tor.exe";
+        private static Client client = null;
 
-
-        public void DownloadSite(string url, string path)
+        public SiteDownloader()
         {
+            if (client == null)
+            {
+                ClientCreateParams createParameters = new ClientCreateParams();
+                createParameters.ConfigurationFile = "";
+                createParameters.ControlPassword = "";
+                createParameters.ControlPort = CONTROL_PORT;
+                createParameters.DefaultConfigurationFile = "";
+                createParameters.Path = PATH;
 
-            /*JUST EXAMPLE WILL BE FIXED */
+                createParameters.SetConfig(ConfigurationNames.AvoidDiskWrites, true);
+                createParameters.SetConfig(ConfigurationNames.GeoIPFile, Path.Combine(Environment.CurrentDirectory, @"Tor\Data\Tor\geoip"));
+                createParameters.SetConfig(ConfigurationNames.GeoIPv6File, Path.Combine(Environment.CurrentDirectory, @"Tor\Data\Tor\geoip6"));
 
-            ClientCreateParams createParameters = new ClientCreateParams();
-            createParameters.ConfigurationFile = "";
-            createParameters.ControlPassword = "";
-            createParameters.ControlPort = CONTROL_PORT;
-            createParameters.DefaultConfigurationFile = "";
-            createParameters.Path = PATH;
-
-            createParameters.SetConfig(ConfigurationNames.AvoidDiskWrites, true);
-            createParameters.SetConfig(ConfigurationNames.GeoIPFile, Path.Combine(Environment.CurrentDirectory, @"Tor\Data\Tor\geoip"));
-            createParameters.SetConfig(ConfigurationNames.GeoIPv6File, Path.Combine(Environment.CurrentDirectory, @"Tor\Data\Tor\geoip6"));
-
-            Client client = Client.Create(createParameters);
-
-            Console.WriteLine(client.Proxy.WebProxy.ToString());
-
-            Console.ReadLine();
+                Client client = Client.Create(createParameters);
+            }
+        }
 
 
+        public string DownloadSite(string url, string path)
+        {
             PhantomJSDriverService service = PhantomJSDriverService.CreateDefaultService();
 
             service.AddArguments(new string[] {
                 "--proxy-type=socks5","--proxy=127.0.0.1:9050"
             });
 
-
-
             using (IWebDriver driver = new PhantomJSDriver(service))
             {
-                driver.Navigate().GoToUrl(@"http://2ip.ru");
-                Console.WriteLine("GoToURL");
-                using (FileStream fs = new FileStream("123.txt", FileMode.Create))
+                try
                 {
-                    using (StreamWriter sw = new StreamWriter(fs))
+                    driver.Navigate().GoToUrl(@"http://2ip.ru");
+                    Console.WriteLine("GoToURL");
+                    using (FileStream fs = new FileStream(path, FileMode.Create))
                     {
-                        sw.Write(driver.PageSource);
+                        using (StreamWriter sw = new StreamWriter(fs))
+                        {
+                            sw.Write(driver.PageSource);
+                        }
                     }
+
+                    return driver.PageSource;
+                }
+                catch(Exception ex)
+                {
+                    /*TO DO*/
+                    return null;
                 }
 
             }
