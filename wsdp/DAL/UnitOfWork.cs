e5ab96072ebac727f43c92703c44e21b826803f2
Slava Tarbinskyi <slava0tarbinskiy@outlook.com>
2016-11-14 12:38:43 +0200

    Moved js and css files to folders.

--- wsdp/DAL/UnitOfWork.cs
+++ wsdp/DAL/UnitOfWork.cs
@@ -6,137 +6,137 @@ using System.Data.Entity.Validation;
 
 namespace DAL
 {
-    public class UnitOfWork : IUnitOfWork, IDisposable
-    {
-        private MainContext context;
+	public class UnitOfWork : IUnitOfWork, IDisposable
+	{
+		private MainContext context;
 
-        #region Private Repositories
+		#region Private Repositories
 
-        private IGenericRepository<User> userRepo;
-        private IGenericRepository<Good> goodRepo;
-        private IGenericRepository<Category> categoryRepo;
-        private IGenericRepository<Property> propertyRepo;
-        private IGenericRepository<WebShop> webShopRepo;
-        private IGenericRepository<Role> roleRepo;
-        private IGenericRepository<ParserTask> parserRepo;
+		private IGenericRepository<User> userRepo;
+		private IGenericRepository<Good> goodRepo;
+		private IGenericRepository<Category> categoryRepo;
+		private IGenericRepository<Property> propertyRepo;
+		private IGenericRepository<WebShop> webShopRepo;
+		private IGenericRepository<Role> roleRepo;
+		private IGenericRepository<ParserTask> parserRepo;
 
-        #endregion Private Repositories
+		#endregion Private Repositories
 
-        public UnitOfWork()
-        {
-            context = new MainContext();
+		public UnitOfWork()
+		{
+			context = new MainContext();
 
-            roleRepo = new GenericRepository<Role>(context);
-            userRepo = new GenericRepository<User>(context);
-            goodRepo = new GenericRepository<Good>(context);
-            categoryRepo = new GenericRepository<Category>(context);
-            propertyRepo = new GenericRepository<Property>(context);
-            webShopRepo = new GenericRepository<WebShop>(context);
-            parserRepo = new GenericRepository<ParserTask>(context);
-        }
+			roleRepo = new GenericRepository<Role>(context);
+			userRepo = new GenericRepository<User>(context);
+			goodRepo = new GenericRepository<Good>(context);
+			categoryRepo = new GenericRepository<Category>(context);
+			propertyRepo = new GenericRepository<Property>(context);
+			webShopRepo = new GenericRepository<WebShop>(context);
+			parserRepo = new GenericRepository<ParserTask>(context);
+		}
 
-        #region Repositories Getters
+		#region Repositories Getters
 
-        public IGenericRepository<Role> RoleRepo
-        {
-            get
-            {
-                if (roleRepo == null) roleRepo = new GenericRepository<Role>(context);
-                return roleRepo;
-            }
-        }
+		public IGenericRepository<Role> RoleRepo
+		{
+			get
+			{
+				if (roleRepo == null) roleRepo = new GenericRepository<Role>(context);
+				return roleRepo;
+			}
+		}
 
-        public IGenericRepository<User> UserRepo
-        {
-            get
-            {
-                if (userRepo == null) userRepo = new GenericRepository<User>(context);
-                return userRepo;
-            }
-        }
+		public IGenericRepository<User> UserRepo
+		{
+			get
+			{
+				if (userRepo == null) userRepo = new GenericRepository<User>(context);
+				return userRepo;
+			}
+		}
 
-        public IGenericRepository<Good> GoodRepo
-        {
-            get
-            {
-                if (goodRepo == null) goodRepo = new GenericRepository<Good>(context);
-                return goodRepo;
-            }
-        }
+		public IGenericRepository<Good> GoodRepo
+		{
+			get
+			{
+				if (goodRepo == null) goodRepo = new GenericRepository<Good>(context);
+				return goodRepo;
+			}
+		}
 
-        public IGenericRepository<Category> CategoryRepo
-        {
-            get
-            {
-                if (categoryRepo == null) categoryRepo = new GenericRepository<Category>(context);
-                return categoryRepo;
-            }
-        }
+		public IGenericRepository<Category> CategoryRepo
+		{
+			get
+			{
+				if (categoryRepo == null) categoryRepo = new GenericRepository<Category>(context);
+				return categoryRepo;
+			}
+		}
 
-        public IGenericRepository<Property> PropertyRepo
-        {
-            get
-            {
-                if (propertyRepo == null) propertyRepo = new GenericRepository<Property>(context);
-                return propertyRepo;
-            }
-        }
+		public IGenericRepository<Property> PropertyRepo
+		{
+			get
+			{
+				if (propertyRepo == null) propertyRepo = new GenericRepository<Property>(context);
+				return propertyRepo;
+			}
+		}
 
-        public IGenericRepository<WebShop> WebShopRepo
-        {
-            get
-            {
-                if (webShopRepo == null) webShopRepo = new GenericRepository<WebShop>(context);
-                return webShopRepo;
-            }
-        }
+		public IGenericRepository<WebShop> WebShopRepo
+		{
+			get
+			{
+				if (webShopRepo == null) webShopRepo = new GenericRepository<WebShop>(context);
+				return webShopRepo;
+			}
+		}
 
-        public IGenericRepository<ParserTask> ParserRepo
-        {
-            get
-            {
-                if (parserRepo == null) parserRepo = new GenericRepository<ParserTask>(context);
-                return parserRepo;
-            }
-        }
+		public IGenericRepository<ParserTask> ParserRepo
+		{
+			get
+			{
+				if (parserRepo == null) parserRepo = new GenericRepository<ParserTask>(context);
+				return parserRepo;
+			}
+		}
 
-        #endregion Repositories Getters
+		#endregion Repositories Getters
 
-        public void Save()
-        {
-            try
-            {
-                context.SaveChanges();
-            }
-            catch (DbEntityValidationException ex)
-            {
-            }
-        }
+		public void Save()
+		{
+			try
+			{
+				context.SaveChanges();
+			}
+			catch (DbEntityValidationException ex)
+			{
+			}
+		}
 
-        #region Dispose
+		#region Dispose
 
-        // https://msdn.microsoft.com/ru-ru/library/system.idisposable(v=vs.110).aspx
+		// https://msdn.microsoft.com/ru-ru/library/system.idisposable(v=vs.110).aspx
 
-        private bool disposed = false;
+		private bool disposed = false;
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!this.disposed)
-            {
-                if (disposing)
-                {
-                    context.Dispose();
-                }
-            }
-            this.disposed = true;
-        }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					context.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 
-        #endregion Dispose
-    }
+		#endregion Dispose
+	}
 }
--- wsdp/WebApp/App_Start/BundleConfig.cs
+++ wsdp/WebApp/App_Start/BundleConfig.cs
@@ -2,89 +2,94 @@
 
 namespace WebApp
 {
-    public class BundleConfig
-    {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+				"~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/EditProperties").Include(
-                    "~/Scripts/bootstrap-combobox.js",
-                    "~/Scripts/EditProperties.js")
-            );
-            bundles.Add(new ScriptBundle("~/bundles/EditUsers").Include(
-                "~/Scripts/jquery.dataTables.min.js",
-                "~/Scripts/dataTables.bootstrap.min.js",
-                "~/Scripts/userConfig.js")
-                );
+			bundles.Add(new ScriptBundle("~/bundles/EditProperties").Include(
+				"~/Scripts/Properties/bootstrap-combobox.js",
+				"~/Scripts/Properties/EditProperties.js")
+				);
+			bundles.Add(new ScriptBundle("~/bundles/EditUsers").Include(
+				"~/Scripts/Users/jquery.dataTables.min.js",
+				"~/Scripts/Users/dataTables.bootstrap.min.js",
+				"~/Scripts/Users/serConfig.js")
+				);
 
-            bundles.Add(new ScriptBundle("~/bundles/WebShop").Include(
-                    "~/Scripts/webshop.js")
-            );
+			bundles.Add(new ScriptBundle("~/bundles/WebShop").Include(
+				"~/Scripts/WebShop/webshop.js")
+				);
 
-            bundles.Add(new ScriptBundle("~/bundles/IteratorPage").Include(
-                "~/Scripts/IteratorPage/IteratorPage.js"
-                ));
+			bundles.Add(new ScriptBundle("~/bundles/IteratorPage").Include(
+				"~/Scripts/IteratorPage/IteratorPage.js"
+				));
 
-            bundles.Add(new ScriptBundle("~/Content/editcategories").Include(
-                    "~/Scripts/editcategories.js",
-                    "~/Scripts/jquery-sortable.js",
-                    "~/Scripts/jquery.treemenu.js"));
+			bundles.Add(new ScriptBundle("~/Content/editcategories").Include(
+				"~/Scripts/Categories/editcategories.js",
+				"~/Scripts/Categories/jquery-sortable.js",
+				"~/Scripts/Categories/jquery.treemenu.js"));
 
-            bundles.Add(new StyleBundle("~/Content/EditUsers_css").Include(
-        "~/Content/dataTables.bootstrap.min.css",
-        "~/Content/EditUsersStylesheet.css"
-        ));
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+				"~/Scripts/JqueryValidate/jquery.validate*"));
 
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+				"~/Scripts/bootstrap/bootstrap.min.js",
+				"~/Scripts/bootstrap/respond.js",
+				"~/Scripts/bootstrap/respond.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/IndexStylesheet").Include(
-        "~/Content/IndexStyleSheet.css"
-        ));
-            bundles.Add(new StyleBundle("~/Content/EditPropertiesStylesheet").Include(
-                    "~/Content/bootstrap.css",
-                    "~/Content/EditPropertiesStylesheet.css",
-                    "~/Content/bootstrap-combobox.css"));
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+				"~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.min.js",
-                "~/Scripts/respond.js",
-                "~/Scripts/respond.min.js"));
+			bundles.Add(new ScriptBundle("~/Content/IFrameJs").Include(
+				"~/Scripts/Frame/Main.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap-select").Include(
+				"~/Scripts/Bootstrap-select/bootstrap-select.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.flaty.min.css"));
+			// Use the development version of Modernizr to develop with and learn from. Then, when you're
+			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 
-            bundles.Add(new StyleBundle("~/Content/PhoneStylesheet").Include(
-                "~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Content/EditUsers_css").Include(
+				"~/Content/Users/dataTables.bootstrap.min.css",
+				"~/Content/Users/EditUsersStylesheet.css"
+				));
+			bundles.Add(new StyleBundle("~/Content/IndexStylesheet").Include(
+				"~/Content/Index/IndexStyleSheet.css"
+				));
+			bundles.Add(new StyleBundle("~/Content/EditPropertiesStylesheet").Include(
+				"~/Content/bootstrap/bootstrap.css",
+				"~/Content/Properties/EditPropertiesStylesheet.css",
+				"~/Content/Properties/bootstrap-combobox.css"));
 
-            bundles.Add(new StyleBundle("~/Content/FridgeStylesheet").Include(
-                "~/Content/FridgeIndex.css"));
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+				"~/Content/bootstrap.flaty.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/TVStylesheet").Include(
-                "~/Content/TVstylesheet.css"));
+			bundles.Add(new StyleBundle("~/Content/PhoneStylesheet").Include(
+				"~/Content/Phone/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/styles").Include(
-                "~/Content/style.css"));
+			bundles.Add(new StyleBundle("~/Content/FridgeStylesheet").Include(
+				"~/Content/Fridge/FridgeIndex.css"));
 
-            bundles.Add(new StyleBundle("~/Content/IFrameStyle").Include(
-                "~/Content/Frame/highlight.css"));
+			bundles.Add(new StyleBundle("~/Content/TVStylesheet").Include(
+				"~/Content/TV/TVstylesheet.css"));
 
-            bundles.Add(new ScriptBundle("~/Content/IFrameJs").Include(
-               "~/Scripts/Frame/Main.js"));
+			bundles.Add(new StyleBundle("~/Content/styles").Include(
+				"~/Content/style.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-select").Include(
-                "~/Scripts/bootstrap-select.min.js"));
+			bundles.Add(new StyleBundle("~/Content/IFrameStyle").Include(
+				"~/Content/Frame/highlight.css"));
 
-            bundles.Add(new StyleBundle("~/Content/bootstrap-select").Include(
-                "~/Content/bootstrap-select.min.css"));
-        }
-    }
+			bundles.Add(new StyleBundle("~/Content/bootstrap-select").Include(
+				"~/Content/bootstrap-select.min.css"));
+
+			bundles.Add(new StyleBundle("~/Content/Categories").Include(
+				"~/Content/Categories/jquery.treemenu.css",
+				"~/Content/Categories/EditCategoriesStylesheet.css"));
+		}
+
+	}
 }
