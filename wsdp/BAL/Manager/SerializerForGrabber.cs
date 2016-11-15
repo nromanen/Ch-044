9e697f1c82390860d2c5c3d4cabb0ebfa7a2cda4
Sasha Odoviichuk <myprogramming96@mail.ru>
2016-11-15 15:54:06 +0200

    Task#25: Add last View and update in DB

---
+++ wsdp/BAL/Manager/SerializerForGrabber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ExtendedXmlSerialization;
+using Model.DTO;
+
+namespace BAL.Manager {
+	public class SerializerForGrabber {
+
+		public object Deserialize(string xml, Type type) {
+			var serializer = new XmlSerializer(type);
+			using (var stream = new StringReader(xml)) {
+				return (GrabberSettingsDTO)serializer.Deserialize(stream);
+			}
+		}
+	}
+}
---
+++ wsdp/Model/DTO/GraberPropertyItemDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Model.DTO {
+	public class GraberPropertyItemDTO {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Value { get; set; }
+	}
+}
