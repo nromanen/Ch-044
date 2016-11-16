e18d2a8bbc6ee0e85a6535ddd9b3700e8817f7d8
StasYakhnenko <ghosteagle07@gmail.com>
2016-11-16 18:44:06 +0200

    merged

---
+++ wsdp/TaskExecuter/Manager/TaskExecuter.cs
@@ -0,0 +1,19 @@
+﻿using BAL.Interface;
+using Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskExecuting.Interface;
+
+namespace TaskExecuting.Manager
+{
+    public class TaskExecuter : ITaskExecuter
+    {
+        public void ExecuteTask(int parsertaskid, string url)
+        {
+
+        }
+    }
+}
