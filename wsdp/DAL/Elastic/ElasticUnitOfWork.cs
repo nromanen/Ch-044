2cd1d63f185c8c2e7fe7f99f5a60dca3e2d33ec7
alisaanosova <alisaanosova@mail.ru>
2016-11-17 15:58:41 +0200

    commit for merge

---
+++ wsdp/DAL/Elastic/ElasticUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Elastic
+{
+    class ElasticUnitOfWork
+    {
+        private ElasticContext context;
+
+        public ElasticUnitOfWork()
+        {
+            context = new ElasticContext();
+            
+        }
+    }
+}
