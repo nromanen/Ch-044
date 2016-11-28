6b7e2ccf1ed1bae125b4e337b7cc224cf6870810
Slava Tarbinskyi <slava0tarbinskiy@outlook.com>
2016-11-28 16:51:42 +0200

    Added HtmlValidator manager.

---
+++ wsdp/BAL/Interface/IHtmlValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Interface
+{
+	public interface IHtmlValidator
+	{
+		string CheckHtml(string html);
+	}
+}
