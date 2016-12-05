60020654848893a8b36371f2fd9b42b02f5fd46a
Sasha Odoviichuk <myprogramming96@mail.ru>
2016-12-05 16:46:22 +0200

    Task #57: add CRUD methods in manager for ExecutingInfo

--- wsdp/BAL/Manager/ExecuteManager.cs
+++ wsdp/BAL/Manager/ExecuteManager.cs
@@ -13,27 +13,36 @@ namespace BAL.Manager {
 	public class ExecuteManager : BaseManager, IExecuteManager {
 
 		public ExecuteManager(IUnitOfWork uOW) : base(uOW) {
-
 		}
+
 		public void Delete(ExecutingInfoDTO info) {
-			throw new NotImplementedException();
+			if (info == null)
+				return;
+			var ExInfo = uOW.ExecuteRepo.GetByID(info.Id);
+			if (ExInfo == null)
+				return;
+			uOW.ExecuteRepo.Delete(ExInfo);
+			uOW.Save();
 		}
 
 		public void DeleteAll() {
-			throw new NotImplementedException();
+			foreach (var item in uOW.ExecuteRepo.All.ToList()) {
+				uOW.ExecuteRepo.Delete(item);
+			}
+			uOW.Save();
 		}
 
 		public IEnumerable<ExecutingInfoDTO> GetAll() {
-			//List<ExecutingInfoDTO> executingsInfo = new List<ExecutingInfoDTO>();
-			//foreach (var webShop in uOW.WebShopRepo.All.Where(x => x.Status)) {
-			//	webShopDto.Add(Mapper.Map<WebShopDTO>(webShop));
-			//}
-			//return webShopDto;
-			return null;
+			List<ExecutingInfoDTO> executingsInfo = new List<ExecutingInfoDTO>();
+			foreach (var info in uOW.ExecuteRepo.All.ToList()) {
+				executingsInfo.Add(Mapper.Map<ExecutingInfoDTO>(info));
+			}
+			return executingsInfo;
 		}
 
 		public ExecutingInfoDTO GetById(int id) {
-			throw new NotImplementedException();
+			var info = uOW.ExecuteRepo.GetByID(id);
+			return info != null ? Mapper.Map<ExecutingInfoDTO>(info) : null;
 		}
 
 		public void Insert(ExecutingInfoDTO info) {
@@ -44,7 +53,16 @@ namespace BAL.Manager {
 		}
 
 		public void Update(ExecutingInfoDTO info) {
-			throw new NotImplementedException();
+			if (info == null)
+				return;
+			var ExInfo = uOW.ExecuteRepo.GetByID(info.Id);
+			if (ExInfo == null)
+				return;
+			ExInfo.Status = info.Status;
+			ExInfo.ParserTask = uOW.ParserRepo.GetByID(info.ParserTaskId);
+			ExInfo.GoodUrl = info.GoodUrl;
+			ExInfo.ErrorMessage = info.ErrorMessage;
+			uOW.Save();
 		}
 	}
 }
