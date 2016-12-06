152ccb2da371824a45c0e7df53005d3a061585c1
Sasha Odoviichuk <myprogramming96@mail.ru>
2016-12-06 16:26:45 +0200

    #43 add IPreviewManager and PreviewManager

---
+++ wsdp/BAL/Interface/IPreviewManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Interface {
+	public interface IPreviewManager {
+		string GetPreviewForOneInput(string url, string xpath);
+	}
+}
