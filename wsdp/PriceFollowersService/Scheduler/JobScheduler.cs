24090062d8910bf0f5cffe19c5485117bcdcf848
Slava Tarbinskyi <slava0tarbinskiy@outlook.com>
2016-12-19 17:27:40 +0200

    Updated JobScheduler

--- wsdp/PriceFollowersService/Scheduler/JobScheduler.cs
+++ wsdp/PriceFollowersService/Scheduler/JobScheduler.cs
@@ -11,77 +11,57 @@ using System.Threading.Tasks;
 
 namespace PriceFollowersService.Scheduler
 {
-    public class JobScheduler : IJob
-    {
-        private UnitOfWork uOw = null;
-        private FollowPriceManager followPriceManager = null;
-        private PriceManager priceManager = null;
-        private EmailService emailService = null;
-        public JobScheduler()
-        {
-            uOw = new UnitOfWork();
-            followPriceManager = new FollowPriceManager(uOw);
-            priceManager = new PriceManager(uOw);
-            emailService = new EmailService(uOw);
-        }
+	public class JobScheduler : IJob
+	{
+		private UnitOfWork uOw = null;
+		private FollowPriceManager followPriceManager = null;
+		private PriceManager priceManager = null;
+		private EmailService emailService = null;
+		public JobScheduler()
+		{
+			uOw = new UnitOfWork();
+			followPriceManager = new FollowPriceManager(uOw);
+			priceManager = new PriceManager(uOw);
+			emailService = new EmailService(uOw);
+		}
 		public void Execute(IJobExecutionContext context)
 		{
 			var email = ConfigurationManager.AppSettings["Email"];
 			var pass = ConfigurationManager.AppSettings["Password"];
 
 			var priceList = priceManager.GetAll();
-			var followedPricesList = followPriceManager.GetAll().Where(i=>i.Status== Common.Enum.FollowStatus.NotSend).ToList();
-			var updatedFollowPrices = followPriceManager.GetAll().Where(i => i.Status == Common.Enum.FollowStatus.Sended && i.Price != null).ToList();
-			foreach (var item in updatedFollowPrices)
+			var followedPricesList = followPriceManager.GetAll();
+
+			foreach (var item in followedPricesList)
 			{
 				var good = uOw.GoodRepo.GetByID(item.Good_Id);
 				if (good != null)
 				{
-					var lastprice = priceList.Where(u => u.Url == good.UrlLink).Select(i=>i.Price).Last();
-					if(lastprice<item.Price)
+					var lastprice = priceList.Where(u => u.Url == good.UrlLink).OrderBy(d => d.Date).Select(i => i.Price).Last();
+					if (lastprice < item.Price)
 					{
-						item.Status = Common.Enum.FollowStatus.NotSend;
 						item.Price = lastprice;
-						followedPricesList.Add(item);
+						followPriceManager.Update(item);
+						var model = item;
+
+						bool result = emailService.SendEmail(model, item.Price, email, pass);
+						if (result == true)
+						{
+							item.Status = Common.Enum.FollowStatus.Sended;
+							followPriceManager.Update(item);
+
+							Console.WriteLine($"Message about price fall for {good.Name} was sended.");
+						}
+						else
+						{
+							Console.WriteLine($"Message for {good.Name} wasn't sended.");
+
+						}
 					}
 				}
+
 			}
-
-            foreach (var item in followedPricesList)
-            {
-                var good = uOw.GoodRepo.GetByID(item.Good_Id);
-                if (good !=null)
-                {
-                    var similarItems = priceList.Where(u => u.Url == good.UrlLink).OrderBy(d => d.Date).ToList();
-                    if (similarItems.Count > 1)
-                    {
-                        var length = similarItems.Count;
-                        var lastItem = similarItems[length-1];
-                        var previous = similarItems[length-2];
-
-                        if (lastItem.Date > previous.Date && lastItem.Price < previous.Price)
-                        {
-                            var price = lastItem.Price;
-                            var model = item;
-
-                            bool result = emailService.SendEmail(model, price, email, pass);
-                            if (result == true)
-                            {
-                                item.Status = Common.Enum.FollowStatus.Sended;
-                                followPriceManager.Update(item);
-
-                                Console.WriteLine($"Message about price fall for {good.Name} was sended.");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Message for {good.Name} wasn't sended.");
-                            }
-                        }
-                    }
-                }
-            }
-
-        }
-    }
+		}
+	}
 }
 
